b936f6423dce73ee9836d76c3b2b087fcc3af643
Felipe <felipetagawaa@gmail.com>
2025-04-20 11:37:17 -0300

    novo usuário

--- types/projeto.d.ts
+++ types/projeto.d.ts
@@ -17,5 +17,17 @@ declare namespace Projeto{
         id?: number;
         descricao: string;
     }
+
+    type PerfilUsuario = {
+        id?: number;
+        perfil: Perfil;
+        usuario: Usuario;
+    }
+
+    type PermissaoPerfilRecurso = {
+        id?: number;
+        perfil: Perfil;
+        recurso: Recurso;
+    }
    
 }
