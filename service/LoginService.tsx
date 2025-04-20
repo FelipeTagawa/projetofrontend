41bdab6f7f3609948129f1bb1132fa985a6a46a7
Felipe <felipetagawaa@gmail.com>
2025-04-20 17:44:53 -0300

    frontend restrição

--- service/LoginService.tsx
+++ service/LoginService.tsx
@@ -10,4 +10,8 @@ export class LoginService{
         return axiosInstance.post("/auth/novoUsuario", usuario)
     }
 
+    login(login:string, senha:string){
+        return axiosInstance.post("auth/login", {username: login, password: senha})
+    }
+
 }
